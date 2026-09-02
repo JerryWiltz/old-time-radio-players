88bdc0c61ebd30657698b18c4db31a6d5780ad81
JerryWiltz <jerry.wiltz@gmail.com>
2026-09-02 10:10:00 -0700

    Add old-time radio players and documentation

---
+++ raw/Whistler.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>The Whistler - OTR</title>
+  <meta name="description" content="The Whistler was an American radio program running for a total of 13 years from May 16, 1942 until September 22, 1955.">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  
+  <style>
+      html,body{margin:0;padding:0}body{font-family:Arial;color:#666;font-size:1.3em}h1{font-weight:normal;font-size:2em;margin-top:0}h2{font-weight:normal;margin:0}a,#search-btn{display:inline-block;padding:12px 12px 12px 0;color:#4c6db7}p a{padding-right:0}p{margin-top:0}a:hover{opacity:.7}a:active{opacity:.5}a:visited{color:#804186}img{border:none;padding:0 20px 8px 0;max-width:100%}img[align=right]{padding-left:20px}.clear{clear:both}header,footer,section{margin:80px}article{margin-bottom:40px}footer span{padding-right:12px}header{position:relative}.logo{display:inline-block;height:68px;line-height:68px;background:url("/old-time-radio.gif") no-repeat left center;background-size:37px auto;padding:0 0 0 50px;font-size:2em;text-decoration:none;margin-bottom:4px}.search{position:absolute;top:0;right:0;max-width:400px;min-width:200px;min-height:40px}#search-btn{display:none;cursor:pointer}nav{margin:0 -5px}nav a{padding:10px 5px}nav a.active{opacity:.6;color:#000;background-color:#f1f1f1;text-decoration:none;cursor:default;pointer-events:none}@media screen and (max-width: 1024px){body{font-size:1.2em}header,footer,section{margin:60px 40px}.search{position:relative;margin:20px 0}}@media screen and (max-width: 700px){body{font-size:1em}h1{font-size:1.4em}header,footer,section{margin:40px 20px}nav{margin:0 -10px}.nomobile{display:none}.logo{font-size:1.8em}.table td:nth-child(3),.table th:nth-child(3),.table td:nth-child(2),.table th:nth-child(2){display:none}#search-form{display:none}#search-btn svg{width:13px;height:13px;display:inline-block;vertical-align:middle;color:#333;text-decoration:none;margin-right:4px}#search-btn{text-decoration:underline;font-size:inherit;padding:10px 5px;display:inline-block;background-color:#fff;border:none}}@media screen and (max-width: 340px){nav a{padding:10px 3px}nav{margin:0 -15px 0 -10px}}.table table{margin-top:20px;border-collapse:collapse;width:100%;padding:0;text-align:left}.table th{background-color:#f1f1f1}.table td,th{border-bottom:1px solid #ccc;padding:12px;margin:0}@media print{footer,#search-form{display:none}}
+  </style>
+  
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <link rel="SHORTCUT ICON" href="/favicon.ico">
+  <meta name="google-site-verification" content="OEi2W7EVOMhveYUkg3wzmQ4r800p0cjYJsNK7iErwbU" />
+
+  <script data-ad-client="ca-pub-0632866227728684" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script async src="https://cse.google.com/cse.js?cx=001483812429665488987:mvaxpape0ia"></script>
+
+</head>
+
+<body>
+
+<header>
+
+  <a class="logo" href="/" title="Old Radio World - Old Time Radio MP3 Downloads">Old Radio World</a>
+
+  <nav>
+    <a class="" href="/">Home</a>
+    <a class="" href="/comedyoldtimeradioshows.php">Comedy</a>
+    <a class="" href="/dramaoldtimeradioshows.php">Drama</a>
+    <a class="active" href="/mysteryoldtimeradioshows.php">Mystery</a>
+    <a class="" href="/scifioldtimeradioshows.php">Sci Fi</a>
+    <a class="" href="/westernoldtimeradioshows.php">Western</a>
+    <a class="" href="/detectiveoldtimeradioshows.php">Detective</a>
+    <a class="" href="/musicoldtimeradioshows.php">Music</a>
+    <a class="nomobile " href="/miscellaneous.php">Miscellaneous</a>
+    <button id="search-btn" onclick="showSearch()";>
+      <svg width="13" height="13" viewBox="0 0 13 13"><title>search</title><path d="m4.8495 7.8226c0.82666 0 1.5262-0.29146 2.0985-0.87438 0.57232-0.58292 0.86378-1.2877 0.87438-2.1144 0.010599-0.82666-0.28086-1.5262-0.87438-2.0985-0.59352-0.57232-1.293-0.86378-2.0985-0.87438-0.8055-0.010599-1.5103 0.28086-2.1144 0.87438-0.60414 0.59352-0.8956 1.293-0.87438 2.0985 0.021197 0.8055 0.31266 1.5103 0.87438 2.1144 0.56172 0.60414 1.2665 0.8956 2.1144 0.87438zm4.4695 0.2115 3.681 3.6819-1.259 1.284-3.6817-3.7 0.0019784-0.69479-0.090043-0.098846c-0.87973 0.76087-1.92 1.1413-3.1207 1.1413-1.3553 0-2.5025-0.46363-3.4417-1.3909s-1.4088-2.0686-1.4088-3.4239c0-1.3553 0.4696-2.4966 1.4088-3.4239 0.9392-0.92727 2.0864-1.3969 3.4417-1.4088 1.3553-0.011889 2.4906 0.45771 3.406 1.4088 0.9154 0.95107 1.379 2.0924 1.3909 3.4239 0 1.2126-0.38043 2.2588-1.1413 3.1385l0.098834 0.090049z"></path></svg>Search
+    </button>
+  </nav>
+
+  <div class="search" id="search-form">
+    <div class="gcse-searchbox-only"></div>
+  </div>
+
+</header>
+
+<main>
+
+
+<section>
+  <h1>The Whistler - 22 Mp3 Downloads Available</h1>
+  <img src="/images/The Whistler.jpg" align=left alt="The Whistler - OTR Picture">  <strong>"The Whistler"</strong> was an American radio program running for a total of 13 years from May 16, 1942 until September 22, 1955.  It was one of the most popular mystery drama's of its' time.  Signal Oil Company sponsored the program.  The marketing catch phrase was: "Let that whistle be your signal for the Signal Oil program, The Whistler."
+<br>
+<br>During the first two years of the show J. Donald Wilson was the writer and producer. During 1944 producer and director George Allen took over.  Other directors included Sterling Tracy and Sherman Marks with final scripts by Joel Malone and Harold Swanton. 692 total episodes were produced.  Over 200 of those episodes can not be found and are no longer in existence. 
+<br>
+<br><strong>Episodes of The Whistler began with the ominous narration:</strong>
+<br>I am the Whistler, and I know many things, for I walk by night. I know many strange tales, many secrets hidden in the hearts of men and women who have stepped into the shadows. I know the nameless terrors of which they dare not speak. 
+<br>
+<br>Those opening words were spoken along with the echo of footsteps and Wilbur Hatch's 13-note theme, whistled weekly by Dorothy Roberts for 13 years. Bill Forman had the title role of host and narrator. Others who portrayed the Whistler at various times were Gale Gordon (Lucille Ball's future television nemesis), Joseph Kearns (played Mr. Wilson on TV series Dennis the Menace), Marvin Miller (soon the announcer for The Bickersons and, later, television's Michael Anthony on The Millionaire), Bill Johnstone (who played The Shadow on radio 1938-1943) and Everett Clarke.
+<br>
+<br>The stories followed an effective formula in which a person's criminal acts were typically undone by their own stupidity. Ironic twist endings were common, as they had been on the show that most obviously influenced it[citation needed], The Shadow. The Whistler narrated, often commenting directly upon the action in the manner of a Greek chorus, taunting the criminal from an omniscient perspective. The Whistler seldom featured any major Hollywood stars, but the quality of writing and performance made it a radio mainstay.  <div class="clear"></div>
+</section>
+
+<section class="table">
+  <h2>Available Episodes</h2>
+
+<table>
+  <tr>
+    <th>Episode Title</th>
+    <th>Broadcast Date</th>
+    <th>File Size</th>
+  </tr>
+
+<tr>
+        <td><a href="/media/The Whistler The Whistler Part 22.mp3">The Whistler Part 22</a></td>
+        <td></td>
+        <td>4.99 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 21.mp3">The Whistler Part 21</a></td>
+        <td></td>
+        <td>6.75 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 20.mp3">The Whistler Part 20</a></td>
+        <td></td>
+        <td>6.52 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 19.mp3">The Whistler Part 19</a></td>
+        <td></td>
+        <td>6.59 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 18.mp3">The Whistler Part 18</a></td>
+        <td></td>
+        <td>6.75 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 17.mp3">The Whistler Part 17</a></td>
+        <td></td>
+        <td>6.75 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 16.mp3">The Whistler Part 16</a></td>
+        <td></td>
+        <td>6.07 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 15.mp3">The Whistler Part 15</a></td>
+        <td></td>
+        <td>6.75 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 14.mp3">The Whistler Part 14</a></td>
+        <td></td>
+        <td>6.75 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 13.mp3">The Whistler Part 13</a></td>
+        <td></td>
+        <td>6.75 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 12.mp3">The Whistler Part 12</a></td>
+        <td></td>
+        <td>6.72 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 11.mp3">The Whistler Part 11</a></td>
+        <td></td>
+        <td>6.84 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 10.mp3">The Whistler Part 10</a></td>
+        <td></td>
+        <td>6.74 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 9.mp3">The Whistler Part 9</a></td>
+        <td></td>
+        <td>6.75 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 8.mp3">The Whistler Part 8</a></td>
+        <td></td>
+        <td>6.73 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 7.mp3">The Whistler Part 7</a></td>
+        <td></td>
+        <td>6.74 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 6.mp3">The Whistler Part 6</a></td>
+        <td></td>
+        <td>6.75 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 5.mp3">The Whistler Part 5</a></td>
+        <td></td>
+        <td>6.73 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 4.mp3">The Whistler Part 4</a></td>
+        <td></td>
+        <td>6.75 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler The Whistler Part 3.mp3">The Whistler Part 3</a></td>
+        <td></td>
+        <td>6.74 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler Cover Up Part 2.mp3">Cover Up Part 2</a></td>
+        <td>Nov 05, 1942</td>
+        <td>6.71 MB</td>
+      </tr>
+    <tr>
+        <td><a href="/media/The Whistler 1942-09-05 House Of Greed Part 01.mp3">House Of Greed Part 01</a></td>
+        <td>Sep 05, 1942</td>
+        <td>4.92 MB</td>
+      </tr>
+    
+</table>
+</section>
+</main>
+
+<footer>
+  <span>OldRadioWorld.com 2003-2026</span>
+  <a href="/about.php">About Us</a>
+  <a href="/contact.php">Contact Us</a>
+  <a href="/privacy-policy.php">Privacy Policy</a>
+</footer>
+
+<script>
+  function showSearch() {
+    document.getElementById('search-form').style.display = 'block';
+    document.getElementById('search-btn').style.display = 'none';
+  }
+</script>
+  
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-2896135-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-2896135-3');
+</script>
+
+</body>
+</html>
